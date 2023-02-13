68b447515c34afa4550a652308397a4fd2ce33da
Diganta Saha <diganta15-4898@diu.edu.bd>
2023-02-13 22:19:05 +0600

    assignment

--- bal.c
+++ bal.c
@@ -1,15 +1,21 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
-    int array[4],count=0;;
-    for(int i=0;i<4;i++)
-     {
-        scanf("%d",&array[i]);
-      if (array[i]>=10)
-       count++;
-     }
-     printf("%d",count);
-
+    int n, target, t, x = 0, y = 1, z = 0;
+    scanf("%d", &n);
+    scanf("%d", &target);
+    while (n != 0)
+    {
+        t = n % 10;
+        n = n / 10;
+        x += t;
+        y *= t;
+        z -= t;
+    }
+    if (x == target || y == target || z == target)
+        printf("Possible");
+    else
+        printf("Imposible");
 
     return 0;
 }
---
+++ noob.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+int main(){
+    int v,t,displacement;
+    scanf("%d %d",&v,&t);
+    displacement=2*v*t;
+    printf("%d",displacement);
+    return 0;
+}
