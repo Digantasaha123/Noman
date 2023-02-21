cf2cf9a6e97db0c9b7c758b2bbd84219b0a52b78
Diganta123 <diganta15-4898@diu.edu.bd>
2023-02-21 23:33:36 +0600

    20 tarikh

---
+++ codechef.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int t;
+scanf("%d",&t);
+while(t--)
+{
+int x,y;
+scanf("%d %d",&x,&y);
+y=10*x;
+if(y>=x)
+{
+    printf("NO\n");
+}
+else if
+{
+    printf("YES\n");
+}
+return 0;
+}
